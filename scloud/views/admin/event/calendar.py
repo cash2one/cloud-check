750a27d449d330cea98c9117dc6f492f7275661b
张鹏 <zhangpeng1@infohold.com.cn>
2016-01-15 16:55:08 +0800

    修改模板

---
+++ scloud/views/admin/event/calendar.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# created: zhangpeng <zhangpeng1@infohold.com.cn>
+
+import scloud
+from torweb.urls import url
+from scloud.shortcuts import *
+from scloud.handlers import Handler
+import requests
+import urlparse
+import urllib
+import urllib2
+import time
+
+
+@url("/event/calendar", name="event.calendar", active="calendar")
+class GuideHandler(Handler):
+    u'日历事件'
+    def get(self):
+        data = {"name": "torweb"}
+        # time.sleep(1)
+        return self.render("admin/event/calendar.html", **data)
