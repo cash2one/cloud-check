583382ecb2b1cb1a9ca0d7fcefa581cd68e1bae1
张鹏 <zhangpeng1@infohold.com.cn>
2016-03-24 14:08:19 +0800

    优化代码

---
+++ scloud/views/admin/apply/base.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from scloud.handlers import AuthHandler
+from scloud.services.svc_project import ProjectService
+from scloud.const import STATUS_RESOURCE
+
+
+class ApplyHandler(AuthHandler):
+    def get_pro_data(self, **kw):
+        # kw = {"pro_id": pro_id}
+        svc = ProjectService(self, kw)
+        if "pro_id" in kw:
+            pro_info_res = svc.get_project()
+            if isinstance(pro_info_res, Exception):
+                raise pro_info_res
+        pro_list_res = svc.get_project_list()
+        data = {
+            "pro_list_res": pro_list_res,
+            "STATUS_RESOURCE": STATUS_RESOURCE,
+        }
+        if "pro_id" in kw:
+            data.update({"pro_info_res": pro_info_res})
+        return data
--- scloud/views/admin/apply/users.py
+++ scloud/views/admin/apply/users.py
@@ -20,51 +20,26 @@ from scloud.services.svc_pro_resource_apply import ProResourceApplyService
 from scloud.async_services import svc_project
 from scloud.utils.unblock import unblock
 from scloud.utils.error import SystemError
-
-
-class GuideStepGetHandler(AuthHandler):
-    def get_pro_info_res(self, pro_id):
-        kw = {"pro_id": pro_id}
-        svc = ProjectService(self, kw)
-        pro_info_res = svc.get_project()
-        if isinstance(pro_info_res, Exception):
-            raise pro_info_res
-        data = {
-            "pro_info_res": pro_info_res,
-            "STATUS_RESOURCE": STATUS_RESOURCE,
-        }
-        return data
+from .base import ApplyHandler
 
 
 @url("/apply/user/index", name="apply.user", active="apply.user")
-class GuideHandler(GuideStepGetHandler):
+class GuideHandler(ApplyHandler):
     u'权限申请'
     @check_perms('pro_info.view')
     @unblock
     def get(self):
-        svc = ProjectService(self)
-        pro_list_res = svc.get_project_list()
-        if pro_list_res.return_code < 0:
-            raise SystemError(pro_list_res.return_code, pro_list_res.return_message)
-        logger.info(pro_list_res)
-        return self.render_to_string("admin/apply/user/index.html", pro_list_res=pro_list_res, STATUS_RESOURCE=STATUS_RESOURCE)
+        data = self.get_pro_data()
+        return self.render_to_string("admin/apply/user/index.html", **data)
 
 
 #@url("/apply/pro_(?P<pro_id>\d+)/user/add", name="apply.user.add", active="apply.user.add")
 @url("/apply/user/add", name="apply.user.add", active="apply.user.add")
-class GuideHandler(GuideStepGetHandler):
+class GuideHandler(ApplyHandler):
     u'权限申请'
     @check_perms('pro_info.view')
     @unblock
     def get(self):
-        svc = ProjectService(self)
         pro_id = self.args.get("pro_id")
-        data = self.get_pro_info_res(pro_id)
-        pro_list_res = svc.get_project_list()
-        if pro_list_res.return_code < 0:
-            raise SystemError(pro_list_res.return_code, pro_list_res.return_message)
-        logger.info(pro_list_res)
-        data.update({
-            "pro_list_res": pro_list_res
-        })
+        data = self.get_pro_data(pro_id=pro_id)
         return self.render_to_string("admin/apply/user/add.html", **data)
